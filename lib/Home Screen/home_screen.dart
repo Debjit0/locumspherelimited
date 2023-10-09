d1d7391f457859381093f19d83f6ff8ea03dc03e
Debjit Sarkar <68617086+Debjit0@users.noreply.github.com>
2023-10-09 22:54:36 +0530

    awdawd

--- lib/Home Screen/home_screen.dart
+++ lib/Home Screen/home_screen.dart
@@ -1,8 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 import 'package:locumspherelimited/Home%20Screen/components/allocation_tile.dart';
 import 'package:locumspherelimited/Models/allocation_model.dart';
+import 'package:locumspherelimited/Profile%20Screen/profile_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -20,6 +23,28 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Allocations"),
+        actions: [
+          InkWell(
+            onTap: () {
+              Get.to(ProfileScreen());
+            },
+            child: Container(
+              height: 40,
+              width: 40,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Color.fromARGB(255, 236, 236, 236)),
+              child: Center(
+                  child: Icon(
+                Icons.person,
+                color: Colors.deepPurple,
+              )),
+            ),
+          ),
+          SizedBox(
+            width: 14,
+          )
+        ],
       ),
       body: StreamBuilder(
           stream: userCollection.doc(uid).collection("Allocations").snapshots(),
